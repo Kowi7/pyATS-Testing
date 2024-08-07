1de6c9027fe6de9318a30829e290ec15eba1429d
Kowi07 <jakobus.l1@gmx.de>
2024-08-07 13:40:34 +0200

    Kommentaränderungen 07.08.2024

--- TestLab/NetzwerkTestAbfrage.py
+++ TestLab/NetzwerkTestAbfrage.py
@@ -7,40 +7,40 @@ from pyats import aetest
 class CheckFiles(aetest.Testcase):
     @aetest.setup
     def setup(self):
-        # Laden des Testbeds aus einer Datei
+        # Load the testbed file
         self.testbed = load('TestLab/testbed.yaml')
-        # Zugriff auf ein spezifisches Gerät
+        # set a specific device in the testbed
         self.device = self.testbed.devices['my_device']
-        # Verbinden mit dem Gerät
+        # connect to the device
         self.device.connect()
 
     @aetest.test
     def list_files(self):
-        # Ausführen des 'ls -la' Befehl aus, um die Dateiliste zu erhalten
+        # execute a command to list all files in the current directory
         output = self.device.execute('ls -la')
         output_directory = 'TestLab/DataDump'
         file_path = os.path.join(output_directory, 'outputDateien.json')
-        os.makedirs(output_directory, exist_ok=True)  # Erstellt den Ordner, falls er nicht existieren sollte
+        os.makedirs(output_directory, exist_ok=True)  # create an output directory if it does not exist
         data = {'output': output}
-        # Speichern der Daten in einer JSON-Datei
+        # save the output in a JSON file
         with open(file_path, 'w') as file:
             json.dump(data, file, indent=4)
 
     @aetest.test
     def list_arp_table(self):
-        # Ausführen des ARP-Befehls, um die ARP-Tabelle zu erhalten
+        # execute a command to list the ARP table
         arp_output = self.device.execute('show arp')
         output_directory = 'TestLab/DataDump'
         arp_path = os.path.join(output_directory, 'arp_table.json')
         os.makedirs(output_directory, exist_ok=True)
         arp_data = {'arp_table': arp_output}
-        # Speichern der ARP-Tabelle in einer JSON-Datei
+        # save the ARP table in a JSON file
         with open(arp_path, 'w') as file:
             json.dump(arp_data, file, indent=4)
 
     @aetest.cleanup
     def cleanup(self):
-        # Trennen der Verbindung
+        # disconnect from the device
         self.device.disconnect()
 
 if __name__ == '__main__':
